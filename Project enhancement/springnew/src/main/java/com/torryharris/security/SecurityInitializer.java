68ea5b3af2b3b4ce3cf41c1b1ddfca46afc19360
ashwinihemegowda <89825917+ashwinihemegowda@users.noreply.github.com>
2021-12-10 11:18:46 +0530

    Delete Project enhancement/springnew directory

--- Project enhancement/springnew/src/main/java/com/torryharris/security/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.torryharris.security;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-@EnableWebSecurity
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
--- Project enhancement/springnew/src/main/java/com/torryharris/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.torryharris.service;
-
-
-import com.torryharris.model.UserRegDTO;
-
-public interface UserService {
-	
-	public boolean registerUser(UserRegDTO user);
-}
